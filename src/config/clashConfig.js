49e8034b21b2bc0f1c685eef5af8d4f305699351
flavedo <1736970762@qq.com>
2026-04-10 11:09:33 +0800

    Update clashConfig.js

--- src/config/clashConfig.js
+++ src/config/clashConfig.js
@@ -9,19 +9,35 @@ export const CLASH_CONFIG = {
 	'allow-lan': false,
 	'mode': 'rule',
 	'log-level': 'info',
+	'geodata-mode': true,
+	'geo-auto-update': false,
+	'geodata-loader': 'standard',
+	'geo-update-interval': 24,
+	'geox-url': {
+		'geoip': 'https://testingcf.jsdelivr.net/gh/MetaCubeX/meta-rules-dat@release/geoip.dat',
+		'geosite': 'https://testingcf.jsdelivr.net/gh/MetaCubeX/meta-rules-dat@release/geosite.dat',
+		'mmdb': 'https://testingcf.jsdelivr.net/gh/MetaCubeX/meta-rules-dat@release/country.mmdb',
+		'asn': 'https://github.com/xishang0128/geoip/releases/download/latest/GeoLite2-ASN.mmdb'
+	},
 	'rule-providers': {
 		// 将由代码自动生成
 	},
 	'dns': {
 		'enable': true,
-		'ipv6': true,
+		'ipv6': false,
 		'respect-rules': true,
 		'enhanced-mode': 'fake-ip',
+		'default-nameserver': [
+			'tcp://223.5.5.5#disable-ipv6=true',
+			'https://120.53.53.53/dns-query',
+			'https://223.5.5.5/dns-query'
+		],
 		'nameserver': [
 			'https://120.53.53.53/dns-query',
 			'https://223.5.5.5/dns-query'
 		],
 		'proxy-server-nameserver': [
+			'https://mydn.chengjason.eu.org/dd',
 			'https://120.53.53.53/dns-query',
 			'https://223.5.5.5/dns-query'
 		],
